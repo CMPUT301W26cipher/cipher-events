5105fd58621ed20d89693abb083e73f9e938327a
“Newthe” <jhe22@ualberta.ca>
2026-03-10 11:11:19 -0600

    Finished QR Code functions for organizer and user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.zxing:core:3.5.3")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
