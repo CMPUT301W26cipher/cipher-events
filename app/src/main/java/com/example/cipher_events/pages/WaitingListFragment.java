5d0523ac4ea2ac7245dcae4b206e4a209b1351bc
Lisa Dsouza <127828762+Lisadsouza07@users.noreply.github.com>
2026-03-12 11:52:02 -0600

    Lisad (#65)
    * Finished User Profile Functions for US 01.02.01 to US 01.02.04
    
    * Finished QR Code functions for organizer and user
    
    * Updated MainActivity.java for User profile and QR Code
    
    * Implemented US 01.05.01 + 01.05.02 + 01.05.03 functions and event page ui
    
    * Updated waiting list UI
    
    ---------
    
    Co-authored-by: “Newthe” <jhe22@ualberta.ca>
    Co-authored-by: Newthe <156024305+Newthe@users.noreply.github.com>
    Co-authored-by: Don Vu <don.p.vu@gmail.com>

---
+++ app/src/main/java/com/example/cipher_events/pages/WaitingListFragment.java
@@ -0,0 +1,26 @@
+package com.example.cipher_events.pages;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cipher_events.R;
+
+public class WaitingListFragment extends Fragment {
+
+    public WaitingListFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.waitinglist, container, false);
+    }
+}
